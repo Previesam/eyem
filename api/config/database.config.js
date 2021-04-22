48ea780bafb8a476a20cd1eb68725c685972f5fe
Previesam <samsmart.ass59@gmail.com>
2021-04-22 17:47:55 +0100

    Oka

--- api/config/database.config.js
+++ api/config/database.config.js
@@ -1,6 +1,6 @@
 var atlas = 'mongodb+srv://sa:Sam@@2015!@cluster0.gmpti.mongodb.net/eyemasters';
-var local = 'mongodb://localhost:27017/eyemasters'
+var local = 'mongodb://localhost:27017/eyemasters';
 
 module.exports = {
-    url: atlas
+    url: local
 }
